e715df3e0874c083393cad92828cc44f2beaac2b
uramuhin <urasfinks@yandex.ru>
2022-06-04 01:05:08 +0300

    ListView pull to refresh badge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+import 'package:badges/badges.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page!!')
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Hello world'),
     );
   }
 }
@@ -24,43 +28,114 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Future<void> _refresh() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.blue[600],
         systemOverlayStyle: const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.blue, // Navigation bar
-          statusBarColor: Colors.blue, // Status bar
+          statusBarColor: Colors.transparent, // Status bar
         ),
-        title: Text(widget.title)
+        title: Text(widget.title),
+        leading: IconButton(
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter'
-            )
-          ]
-        )
+        child: LiquidPullToRefresh(
+          color: Colors.blue[600],
+          showChildOpacityTransition: false,
+          springAnimationDurationInMilliseconds: 500,
+          animSpeedFactor: 2,
+          height: 90,
+          onRefresh: _refresh,
+          child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(8),
+            itemCount: 50,
+            itemBuilder: (BuildContext context, int index) {
+              return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 10, 0),
+                      child: const CircleAvatar(
+                        backgroundImage:
+                            NetworkImage('https://i.stack.imgur.com/Qt4JP.png'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: const <Widget>[
+                                Text(
+                                  'If you want the same background color, you should use a FlatButton instead, and Colors.transparent to avoid the elevation shadow from the RaisedButton',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                Text('Description',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 13)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Icon(
+                      Icons.arrow_forward_ios_sharp,
+                      color: Colors.grey,
+                      size: 17,
+                    ),
+                  ]);
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add)
-      )
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        items: [
+          const BottomNavigationBarItem(
+              label: 'Главная', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+            icon: Badge(
+                position: BadgePosition.topEnd(top: -5, end: -22),
+                shape: BadgeShape.square,
+                borderRadius: BorderRadius.circular(10),
+                badgeContent: const Text(
+                  '4500',
+                  style: TextStyle(color: Colors.white, fontSize: 8),
+                ),
+                child: const Icon(Icons.business)),
+            label: 'Организация',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Аккаунт',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {},
+      ),
     );
   }
 }
