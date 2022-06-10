a61acddf1f04901edbab09eddfe417d65a299088
uramuhin <urasfinks@yandex.ru>
2022-06-10 19:39:46 +0300

    Revert "AppWrite add"
    This reverts commit 4d8c6e566cad11028b90b1bbc4c8b176af8f54a7.

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,8 @@ import 'AppStore/AppStore.dart';
 import 'TabWrap.dart';
 import 'package:appwrite/appwrite.dart';
 
-Future<void> appWrite() async {
-  Client client = Client();
-
-  client
-          .setEndpoint('http://jamsys.ru/v1') // Your Appwrite Endpoint
-          .setProject('62a31fb742577aae60b1') // Your project ID
-          .setSelfSigned(
-              status:
-                  true) // For self signed certificates, only use for development
-      ;
-  Account account = Account(client);
-
-  final user = await account.create(
-      userId: 'unique()',
-      email: 'me@appwrite.io',
-      password: 'password',
-      name: 'My Name');
-
-  print(user);
-
-  final realtime = Realtime(client);
-  final subscription = realtime.subscribe(['files']);
-
-  subscription.stream.listen((response) {
-    print(response);
-  });
-}
-
 void main() {
   runApp(const RouterPage());
-  appWrite();
 }
 
 class RouterPage extends StatelessWidget {
